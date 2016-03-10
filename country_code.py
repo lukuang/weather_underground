a64d8807064154a8c946b8cc712240920f36b918
lukuang <lukuang1989@gmail.com>
2016-03-10 17:09:15 -0500

    fix bug

--- country_code.py
+++ country_code.py
@@ -22,7 +22,7 @@ class Country_Code(object):
         else:
             self._name = resource_filename('weather_underground', "mapping")
 
-        self._mapping =json.load(open(self.name))
+        self._mapping =json.load(open(self._name))
 
     @staticmethod            
     def get_country_iso_code(name):
